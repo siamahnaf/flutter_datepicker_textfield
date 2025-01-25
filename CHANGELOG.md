53b9484bc1fa7e221988b5ace859dedc9d6dd00b
Siam Ahnaf <siamahnaf198@gmail.com>
2025-01-26 00:07:58 +0600

    1.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 1.0.5
 
-* TODO: This is initial release
+* Bug fixing
---
+++ example/date_picker_example.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datepicker_textfield/date_picker_controller.dart';
+import 'package:flutter_datepicker_textfield/flutter_datepicker_textfield.dart';
+
+class DatePickerExample extends StatelessWidget {
+  final String? Function(String?)? validator;
+  final Function(DateTime)? onChanged;
+  final DateTime? initialValue;
+  final DatePickerController? controller;
+  const DatePickerExample({
+    super.key,
+    this.controller,
+    this.initialValue,
+    this.onChanged,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DatepickerTextfield(
+      validator: validator,
+      onChanged: onChanged,
+      initialValue: initialValue,
+      controller: controller,
+      textFieldDecoration: InputDecoration(
+        hintText: '',
+        suffixIcon: const Padding(
+          padding: EdgeInsets.only(right: 5, left: 8),
+          child: Icon(
+            Icons.calendar_month,
+          ),
+        ),
+        filled: false,
+      ),
+    );
+  }
+}
